0cf55c006a98a23cb17f70eae4e3bdfa07c29b13
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-07-12 17:43:15 +0200

    Add dataset preprocessing

--- tests.R
+++ tests.R
@@ -2,3 +2,15 @@
 # Objective : Comparing dimensionality reduction algorithms for mixed data
 # Created by: Alessandro Serpi - 1647244
 # Created on: 2020-07-01
+
+caravan <- ISLR::Caravan
+caravan$MOSTYPE <- factor(caravan$MOSTYPE)
+caravan$MOSHOOFD <- factor(caravan$MOSHOOFD)
+caravan$MGODRK <- factor(caravan$MGODRK)
+caravan$PWAPART <- factor(caravan$PWAPART)
+
+wine <- read.csv("datasets/winequality-red.csv", sep=";")
+wine$quality <- factor(wine$quality, levels = 1:10)
+
+data <- caravan
+n_dims <- Inf
