635dc64205127272955dbeede2fa1bd09df82776
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-07-19 14:52:02 +0200

    Add PRINCIPALS

---
+++ principals.R
@@ -0,0 +1,8 @@
+# Title     : PRINCIPALS
+# Objective : Implementation of PRINCIPALS (Young et al., 1978)
+# Created by: Alessandro Serpi
+# Created on: 18/07/2020
+
+principals <- function (X, r = Inf) {
+  # TODO
+}
