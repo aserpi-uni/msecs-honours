708fca0254f9f1fc3f52b1b7c0b6200893374f1b
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-10-18 20:14:12 +0200

    Vary dataset size when benchmarking times

--- benchmark.R
+++ benchmark.R
@@ -6,43 +6,58 @@
 library("tidyverse")
 library("ggfortify")
 library("autoplotly")
+library("bench")
 
 source("dim_red_utils.R")
 source("mixed_datasets.R")
 
-data_name <- "caravan"
+
+caravan <- caravan()
 n_dims <- Inf
+all_times <- data.frame(matrix(ncol = 3, nrow = 0))
+colnames(all_times) <- c("algorithm", "samples", "exec_time")
 
+all_results <- press(
+  samples = floor(nrow(caravan)/9) * 1:9,
+  rep = 1:100,
+  {
+    data <- caravan[sample(5822, rep),]
+    result <- mark(
+      ade4 = ade4_wrapper(data, n_dims),
+      FAMD = famd_wrapper(data, n_dims),
+      PCAmixdata = pcamix_wrapper(pcamix_pre(data), n_dims),
+      PCA_1hot = pca_one_hot_wrapper(pca_one_hot_pre(data)),
+      iterations = 100,
+      check = FALSE,
+      filter_gc = TRUE,
+      relative = FALSE,
+      time_unit = "ms"
+    )
 
-data <- do.call(data_name, list())
-result <- bench::mark(
-  ade4 = ade4_wrapper(data, n_dims),
-  FAMD = famd_wrapper(data, n_dims),
-  PCAmixdata = pcamix_wrapper(pcamix_pre(data), n_dims),
-  PCA_1hot = pca_one_hot_wrapper(pca_one_hot_pre(data)),
-  iterations = 10,
-  check = FALSE,
-  filter_gc = TRUE,
-  relative = FALSE,
-  time_unit = "ms"
+    names <- attr(result$expression, "description")
+    result_times <- do.call(cbind, result$time)
+    colnames(result_times) <- names
+    result_times <- pivot_longer(
+      data.frame(result_times),
+      cols = all_of(names),
+      names_to = "algorithm",
+      values_to = "exec_time",
+      names_transform = list(algorithm = ~parse_factor(.x, levels = names))
+    )
+
+    all_times <- rbind(all_times, cbind(samples, result_times))
+    write_result(result_times, "caravan", str_interp("${samples}_${rep}_times"))
+
+    result
+  }
 )
-names <- attr(result$expression, "description")
-result_times <- do.call(cbind, result$time)
-colnames(result_times) <- names
-result_times <- pivot_longer(
-  data.frame(result_times),
-  cols = all_of(names),
-  names_to = "algorithm",
-  values_to = "exec_time",
-  names_transform = list(algorithm = ~parse_factor(.x, levels = names))
-)
 
-p <- ggplot(result_times, aes(x = algorithm, y = exec_time, fill = algorithm)) +
+save(all_results, "out/all_results.Rda")
+write_result(all_times, "caravan", "times")
+
+p <- ggplot(all_times, aes(x = algorithm, y = exec_time, fill = samples)) +
   geom_violin(trim = TRUE) +
-  geom_boxplot(colour = "black", fill = "transparent", width = .5) +
   scale_fill_brewer(palette = "Dark2") +
   labs(title = "Execution times", x = "Algorithm", y = "Execution time [s]") +
   theme(legend.position = "none")
 autoplotly(p)
-
-write_result(result_times, data_name, "times_2")
