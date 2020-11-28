eabdeeac2451f813e627432e287c53e7f6b923b5
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-11-28 21:08:34 +0100

    Improve sampling

--- benchmark.R
+++ benchmark.R
@@ -37,36 +37,36 @@ bench_samples <- function(data_name, n_dims, n_samples, n_iter, n_reps) {
   main_dataset <- do.call(data_name, list())
   reps <- 1:n_reps
   samples <- floor(nrow(main_dataset) / n_samples) * 1:n_samples
-source_filename <- "out/results_${data_name}_${sample}_${rep}.rds"
+  source_filename <- "out/results_${data_name}_${sample}_${rep}.rds"
 
-message("Repetition\t\tSamples\t\t\tTime")
-for (rep in reps) {
-  for (sample in samples) {
-    message(rep, "\t\t\t\t", sample, "\t\t\t", Sys.time())
-    data <- main_dataset[sample(nrow(main_dataset), sample),]
-    data <- data[data %>%  # Select only columns which do not contain all identical cells
-                   summarise_all(~n_distinct(.)) %>%
-                   select_if(. != 1) %>%
-                   colnames()
-    ]
+  message("Repetition\t\tSamples\t\t\tTime")
+  for (rep in reps) {
+    for (sample in samples) {
+      message(rep, "\t\t\t\t", sample, "\t\t\t", Sys.time())
+      data <- slice_sample(main_dataset, sample)
+      data <- data[data %>%  # Select only columns which do not contain all identical cells
+                     summarise_all(~n_distinct(.)) %>%
+                     select_if(. != 1) %>%
+                     colnames()
+      ]
 
-    results <- mark(
-      ade4 = ade4_wrapper(data, n_dims),
-      FAMD = famd_wrapper(data, n_dims),
-      PCAmixdata = pcamix_wrapper(pcamix_pre(data), n_dims),
-      PCA_1hot = pca_one_hot_wrapper(pca_one_hot_pre(data)),
+      results <- mark(
+        ade4 = ade4_wrapper(data, n_dims),
+        FAMD = famd_wrapper(data, n_dims),
+        PCAmixdata = pcamix_wrapper(pcamix_pre(data), n_dims),
+        PCA_1hot = pca_one_hot_wrapper(pca_one_hot_pre(data)),
         iterations = n_iter,
-      check = FALSE,
-      filter_gc = FALSE,
-      relative = FALSE,
+        check = FALSE,
+        filter_gc = FALSE,
+        relative = FALSE,
         time_unit = "ms"
       )
       saveRDS(results, file = str_interp(source_filename))
 
       # Remove old elements from memory
       rm(data)
-    rm(results)
-    gc()
+      rm(results)
+      gc()
     }
   }
 
@@ -77,16 +77,16 @@ for (rep in reps) {
     for (sample in samples) {
       results <- readRDS(file = str_interp(source_filename))
 
-    names <- attr(results$expression, "description")
-    result_times <- do.call(cbind, results$time)
-    colnames(result_times) <- names
-    result_times <- pivot_longer(
-      data.frame(result_times),
-      cols = all_of(names),
-      names_to = "algorithm",
-      values_to = "exec_time",
-      names_transform = list(algorithm = ~parse_factor(.x, levels = names))
-    )
+      names <- attr(results$expression, "description")
+      result_times <- do.call(cbind, results$time)
+      colnames(result_times) <- names
+      result_times <- pivot_longer(
+        data.frame(result_times),
+        cols = all_of(names),
+        names_to = "algorithm",
+        values_to = "exec_time",
+        names_transform = list(algorithm = ~parse_factor(.x, levels = names))
+      )
 
       result_gc <- extract_gc(results, names, n_iter)
       all_times <- rbind(all_times, cbind(sample, result_times, result_gc))
@@ -98,5 +98,5 @@ for (rep in reps) {
   p <- ggplot(all_times, aes(algorithm, exec_time, color = result_gc)) +
     ggbeeswarm::geom_beeswarm() +
     facet_wrap(vars(sample), ncol = 3)
-autoplotly(p)
+  autoplotly(p)
 }
